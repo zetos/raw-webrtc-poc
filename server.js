94d427cf3ce37d74e423ee42f6bbe39e91e0d092
Zetos Lima <cesar_manoel@hotmail.com>
2021-01-17 19:45:15 -0300

    feat: server setup

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+const socket = require('socket.io');
+const io = socket(server);
+
+const rooms = {}; // TODO: change to a cache db.
+
+io.on('connection', (socket) => {
+  socket.on('join room', (roomID) => {
+    if (rooms[roomID]) {
+      rooms[roomID].push(socket.id);
+    } else {
+      rooms[roomID] = [socket.id];
+    }
+    const otherUser = rooms[roomID].find((id) => id !== socket.id); // TODO: change this logic to allow multiple users.
+    if (otherUser) {
+      socket.emit('other user', otherUser);
+      socket.to(otherUser).emit('user joined', socket.id);
+    }
+  });
+
+  socket.on('offer', (payload) => {
+    io.to(payload.target).emit('offer', payload);
+  });
+
+  socket.on('answer', (payload) => {
+    io.to(payload.target).emit('answer', payload);
+  });
+
+  socket.on('ice-candidate', (incoming) => {
+    io.to(incoming.target).emit('ice-candidate', incoming.candidate);
+  });
+});
+
+server.listen(8000, () => console.log('server is running on port 8000'));
